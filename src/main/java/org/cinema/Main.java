0366878f752c4323ac80544832c9f3cf574b7718
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-01-22 12:36:37 +0100

    Task 3 - Cinema Employee, final version

--- src/main/java/org/cinema/Main.java
+++ src/main/java/org/cinema/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        TicketSeller ticketSeller = new TicketSeller("Anna", "Nowak", 2019, 3000);
+        TicketSeller ticketSeller = new TicketSeller("Anna","Nowak", 2019, 3000);
         System.out.println(ticketSeller.introduce());
 
         Manager manager = new Manager("Jan", "Kowalski", 2015, 5000, 650);
