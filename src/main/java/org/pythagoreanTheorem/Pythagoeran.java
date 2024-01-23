e6430167841d1c548fef4d6b42d7b2d107f08a3f
T.Abramczyk1126 <t.abramczyk@gmail.com>
2024-01-23 23:53:28 +0100

    Task 5 - Rectangular Triangle

--- src/main/java/org/pythagoreanTheorem/Pythagoeran.java
+++ src/main/java/org/pythagoreanTheorem/Pythagoeran.java
@@ -3,10 +3,6 @@ package org.pythagoreanTheorem;
 public class Pythagoeran {
 
     public static boolean rectangularTriangle(double a, double b, double c) {
-        if (c == Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))) {
-            return true;
-        } else {
-            return false;
-        }
+        return c == Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
     }
 }
